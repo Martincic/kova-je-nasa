349c085a8c6c0cdb897720fcd765f05b04221380
Martincic <martincic.tomas@gmail.com>
2021-02-06 11:00:53 +0100

    live reading, refactor required

--- Sensors.py
+++ Sensors.py
@@ -18,12 +18,10 @@ class Sensors:
         #etc..
         
     def readTempHumid(self):
-        for x in range(5): #DHT is not constant output
-            sleep(0.1)
-            humidity, temperature = Adafruit_DHT.read(self.DHT_SENSOR, self.DHT_PIN)
-            if humidity is not None and temperature is not None:
-                self.answers['temp'] = temperature
-                self.answers['humid'] = humidity
+        humidity, temperature = Adafruit_DHT.read(self.DHT_SENSOR, self.DHT_PIN)
+        if humidity is not None and temperature is not None:
+            self.answers['temp'] = temperature
+            self.answers['humid'] = humidity
 #                print("Temp:", self.getAnswer('temp'))
  #               print("Humid:", self.getAnswer('humid'))
         return humidity, temperature
