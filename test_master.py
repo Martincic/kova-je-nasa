c05263420661d73ad73deda7ebbf23e18bcd28ff
Martincic <martincic.tomas@gmail.com>
2021-02-06 12:01:40 +0100

    idiot

--- test_master.py
+++ test_master.py
@@ -44,20 +44,21 @@ def askQuestion(question, count=5):  # count = times question is asked
             print("send() failed or timed out")
         else:  # sent successful; listen for a response
             nrf.listen = True  # get radio ready to receive a response
-            timeout = time.monotonic_ns() + 200000000  # set sentinal for timeout
+            timeout = time.monotonic_ns() + 1000000000  # set sentinal for timeout
             while not nrf.available() and time.monotonic_ns() < timeout:
-                # this loop hangs for 200 ms or until response is received
+                # this loop waits 1s for slave to read data and send back
                 pass
             nrf.listen = False  # put the radio back in TX mode
             end_timer = time.monotonic_ns()  # stop timer
-            print(
-                "Transmission successful! Sent: {}".format(buffer.decode("utf-8"),),
-                end=" ",
-            )
+            
             if nrf.pipe is None:  # is there a payload?
                 # nrf.pipe is also updated using `nrf.listen = False`
-                print("Received no response.")
+                pass
             else:
+                print(
+                "Transmission successful! Sent: {}".format(buffer.decode("utf-8"),),
+                end=" ",
+                )
                 length = nrf.any()  # reset with read()
                 pipe_number = nrf.pipe  # reset with read()
                 received = nrf.read()  # grab the response & return it
